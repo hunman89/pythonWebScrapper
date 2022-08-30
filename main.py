69ccc04f0629510cb232ad3146ffbd78cd8e1292
SungHun Jung <hunman89@gmail.com>
2022-08-30 03:12:58 +0000

    job scrapper 1

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from requests import get
+from bs4 import BeautifulSoup
 
-websites = [
-    "google.com",
-    "airbnb.com",
-    "https://twitter.com",
-    "facebook.com",
-    "https://tiktok.com"
-]
+base_url = 'https://weworkremotely.com/remote-jobs/search?utf8=✓&term='
+search_term = 'python'
+response = get(f'{base_url}{search_term}')
 
-for website in websites:
-    if not website.startswith("https://"):
-        website = f'https://{website}'
-    response = get(website)
-    print(response.status_code)
+if response.status_code != 200:
+    print('request error')
+else:    
+    results = []
+    soup = BeautifulSoup(response.text, 'html.parser')
+    jobs = soup.find_all('section', class_='jobs')
+    for job_sections in jobs:
+        job_posts = job_sections.find_all('li')
+        job_posts.pop(-1)
+        for job_post in job_posts:
+            anchors = job_post.find_all('a')
+            anchor = anchors[1]
+            link = anchor['href']
+            company, kind, region = anchor.find_all('span', class_='company')
+            title = anchor.find('span', class_='title')
+            job_data = {
+                'company' : company.string,
+                'region' : region.string,
+                'title' : title.string
+            }
+            results.append(job_data)
+    print(results)
